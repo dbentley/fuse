4c41c257d50b4d93e4c35da204e9bd92a95d6225
Dan Bentley <dbentley@twitter.com>
2016-05-16 11:10:10 -0400

    fuse no-copy reqs/resps

--- memory.go
+++ memory.go
@@ -9,95 +9,40 @@ import (
 // Maximum file write size we are prepared to receive from the kernel.
 const maxWrite = 16 * 1024 * 1024
 
+// TODO(dbentley): Why does this seem reasonable?
+const maxReadSize = maxWrite
+
 // All requests read from the kernel, without data, are shorter than
 // this.
 var maxRequestSize = syscall.Getpagesize()
-var bufSize = maxRequestSize + maxWrite
+var bufSize = uintptr(maxRequestSize + maxWrite)
 
-type Buffers struct {
-	// Raw []bytes
-	inBytes  []byte
-	outBytes []byte
+// Our allocator requests this much. This is ~50MB
+var memSize = 3 * bufSize
 
-	reqBytes  []byte
-	respBytes []byte
-
-	allocBytes []byte
-
-	// Higher-level but still bytes
-	reqMsg  message
-	respBuf buffer
-
-	alloc allocator
-	scope RequestScope
+func MakeAlloc() *Allocator {
+	return &Allocator{buf: make([]byte, memSize)}
 }
 
-func MakeBuffers() (r *Buffers) {
-	r = new(Buffers)
-	r.inBytes = make([]byte, bufSize)
-	r.outBytes = make([]byte, bufSize)
-	// This is way too many bytes for what we need
-	r.reqBytes = make([]byte, bufSize)
-	r.respBytes = make([]byte, bufSize)
-	r.allocBytes = make([]byte, bufSize)
-	r.alloc.init(r.allocBytes)
-	r.scope.bufs = r
-	return r
+func (a *Allocator) newRequest(c *Conn) *RequestScope {
+	a.reset()
+
+	scope := requestScope(a)
+	scope.alloc = a
+	scope.conn = c
+	return scope
 }
 
-func (b *Buffers) Reset() *RequestScope {
-	clear := b.inBytes[0:len(b.reqMsg.buf)]
-	for i := range clear {
-		clear[i] = 0
-	}
-
-	clear = b.outBytes[0:len(b.respBuf)]
-	for i := range clear {
-		clear[i] = 0
-	}
-
-	b.reqMsg.buf = b.inBytes[:]
-	b.reqMsg.hdr = (*inHeader)(unsafe.Pointer(&b.inBytes[0]))
-	newBuffer(&b.respBuf, b.outBytes)
-
-	b.alloc.reset()
-
-	b.scope.Req = nil
-	b.scope.reqMsg = &b.reqMsg
-	b.scope.respBuf = &b.respBuf
-	b.scope.conn = nil
-	b.scope.Alloc = &b.alloc
-	b.scope.responded = false
-
-	return &b.scope
+func requestScope(a *Allocator) *RequestScope {
+	return (*RequestScope)(a.allocPointer(requestScopeSize))
 }
 
-type RequestScope struct {
-	Req       Request
-	reqMsg    *message
-	respBuf   *buffer
-	bufs      *Buffers
-	conn      *Conn
-	Alloc     *allocator
-	responded bool
-}
-
-type Allocator interface {
-	Alloc(size int) []byte
-	Free(size int)
-}
-
-type allocator struct {
+type Allocator struct {
 	buf  []byte
 	next int
 }
 
-func (a *allocator) init(buf []byte) {
-	a.buf = buf
-	a.next = 0
-}
-
-func (a *allocator) Alloc(size int) []byte {
+func (a *Allocator) alloc(size uintptr) []byte {
 	s := int(size)
 	if a.next+s > cap(a.buf) {
 		panic(fmt.Sprintf("Not enough capacity: %v + %v > %v", a.next, size, cap(a.buf)))
@@ -107,14 +52,18 @@ func (a *allocator) Alloc(size int) []byte {
 	return r
 }
 
-func (a *allocator) Free(size int) {
+func (a *Allocator) allocPointer(size uintptr) unsafe.Pointer {
+	return unsafe.Pointer(&a.alloc(size)[0])
+}
+
+func (a *Allocator) free(size int) {
 	a.next -= size
 	if a.next < 0 {
 		panic(fmt.Sprintf("allocator.next below 0: %v %v", a.next, size))
 	}
 }
 
-func (a *allocator) reset() {
+func (a *Allocator) reset() {
 	b := a.buf[0:a.next]
 	for idx := range b {
 		b[idx] = 0
@@ -122,36 +71,15 @@ func (a *allocator) reset() {
 	a.next = 0
 }
 
-// buffer provides a mechanism to write out a message in segments. This is useful because
-// the fuse protocol means we have to assemble one contiguous byte array with several headers
-// and then data.
-type buffer []byte
-
-func newBuffer(buf *buffer, bytes []byte) {
-	*buf = bytes[0:0]
+type RequestScope struct {
+	conn  *Conn
+	alloc *Allocator
+	Req   Request
+	Resp  Response
 }
 
-// newSegment creates a new segment of newSize bytes and returns a pointer to the new segment.
-func (w *buffer) newSegment(newSize uintptr) unsafe.Pointer {
-	return unsafe.Pointer(&(w.newSlice(newSize)[0]))
-}
+const requestScopeSize = unsafe.Sizeof(RequestScope{})
 
-func (w *buffer) newSlice(newSize uintptr) []byte {
-	s := int(newSize)
-	if len(*w)+s > cap(*w) {
-		panic(fmt.Sprintf("Not enough capacity: %v + %v > %v", len(*w), newSize, cap(*w)))
-	}
-	l := len(*w)
-	*w = (*w)[:l+s]
-	return (*w)[l : l+s]
-}
-
-func (w *buffer) truncate(extra int) {
-	l := len(*w)
-	clear := (*w)[l-extra:]
-	for i := range clear {
-		clear[i] = 0
-	}
-
-	*w = (*w)[:l-extra]
+func (s *RequestScope) Release() {
+	// TODO(dbentley): here is where we could respond if we haven't responded yet
 }
